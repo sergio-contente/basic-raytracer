57095d70bf2f2f251ae0b1501edef478b8c94d53
sergio-contente <sergio.mcontente@gmail.com>
2022-12-26 16:28:32 -0300

    🚧  Implement main methods for vector operations

--- inc/utils/utils.h
+++ inc/utils/utils.h
@@ -12,7 +12,21 @@ class Vec3
 public:
 	V x, y, z;
 	Vec3();
+	Vec3( V x, V y, V z );
 	~Vec3();
+	V norma() const;
+	V prod_interno(const Vec3< V > &v) const;
+	Vec3<V> operator * (const V &f) const;
+	Vec3<V> operator * (const Vec3<V> &v) const;
+	Vec3<V> operator - (const Vec3<V> &v) const;
+	Vec3<V> operator + (const Vec3<V> &v) const;
+	Vec3<V> operator += (const Vec3<V> &v) const;
+	Vec3<V> operator *= (const Vec3<V> &v) const;
+	friend std::ostream & operator << (std::ostream &os, const Vec3<V> &v)
+    {
+        os << "[" << v.x << " " << v.y << " " << v.z << "]";
+        return os;
+    }
 };
 
 #endif
--- src/lib/utils.cpp
+++ src/lib/utils.cpp
@@ -1,11 +1,66 @@
 #include "../../inc/utils/utils.h"
 
 template< typename V >
-Vec3< V >::Vec3()
+Vec3< V >::Vec3() : x(V(0)), y(V(0)), z(V(0))
+{
+}
+
+template< typename V >
+Vec3< V >::Vec3(V x, V y, V z) : this-> x(x), this->y(y), this->z(z)
 {
 }
 
 template< typename V >
 Vec3< V >::~Vec3()
 {
+}
+
+template< typename V >
+V Vec3< V >::norma() const
+{
+	return sqrt(x*x + y*y + z*z);
+}
+
+template< typename V >
+V Vec3< V >::prod_interno(const Vec3< V > &v) const
+{
+	return x*v.x + y*v.y + z*v.z;
+}
+
+template< typename V >
+Vec3<V> Vec3< V > :: operator * (const V &f) const
+{
+	return Vec3<V>(x * f, y * f, z * f);
+}
+
+template< typename V >
+Vec3<V> Vec3<V> :: operator * (const Vec3<V> &v) const
+{
+return Vec3<V>(x * v.x, y * v.y, z * v.z);
+}
+
+template< typename V >
+Vec3<V> Vec3<V> :: operator - (const Vec3<V> &v) const
+{
+return Vec3<V>(x - v.x, y - v.y, z - v.z);
+}
+
+template< typename V >
+Vec3<V> Vec3<V> :: operator + (const Vec3<V> &v) const
+{
+return Vec3<V>(x + v.x, y + v.y, z + v.z);
+}
+
+template< typename V >
+Vec3<V> Vec3<V> :: operator += (const Vec3<V> &v) const
+{
+ 	x += v.x, y += v.y, z += v.z;
+	return *this;
+}
+
+template< typename V >
+Vec3<V> Vec3<V> :: operator *= (const Vec3<V> &v) const
+{
+ 	x *= v.x, y *= v.y, z *= v.z;
+	return *this;
 }
