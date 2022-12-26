4b41e0ccaa76a2f4721236883a635f8904f8de2a
sergio-contente <sergio.mcontente@gmail.com>
2022-12-26 17:18:55 -0300

    🚀  Passes all tests!

--- src/bin/test_vector.cpp
+++ src/bin/test_vector.cpp
@@ -7,7 +7,20 @@ int main(int argc, const char** argv) {
 	vetor.x = 0.1;
 	vetor.y = 2;
 	vetor.z = 3.14;
-	
+	float f = 2.0;
+	std::cout <<  "x: " << vetor.x  << " y: " << vetor.y << " z: " << vetor.z << std::endl;
+	vetor = vetor * f;
+	std::cout <<  "x: " << vetor.x  << " y: " << vetor.y << " z: " << vetor.z << std::endl;
+	vetor = vetor * 0.5;
+	Vec3f vetor2;
+	vetor2.x = 1.0;
+	vetor2.y = 3;
+	vetor2.z = -3.14;
+	vetor = vetor + vetor2;
+	std::cout <<  "x: " << vetor.x  << " y: " << vetor.y << " z: " << vetor.z << std::endl;
+	float norm = vetor.norma();
+	std::cout <<  "norma: " << norm  << std::endl;
+	vetor = vetor.normalize();
 	std::cout <<  "x: " << vetor.x  << " y: " << vetor.y << " z: " << vetor.z << std::endl;
 
 	return 0;
