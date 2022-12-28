f22df2528a6f3f0db4a787825bc52315ba8106ac
sergio-contente <sergio.mcontente@gmail.com>
2022-12-28 15:37:03 -0300

    🚧 Need to learn about fresnel effect now

--- src/lib/sphere.cpp
+++ src/lib/sphere.cpp
@@ -1,8 +1,8 @@
 #include "../../inc/utils/sphere.h"
 
 Sphere::Sphere(const Vec3f &center, const float &radius, const Vec3f &surface_color,
-				const Vec3f &emission_color, const float &k_transparency = 0,
-				const float &k_reflection = 0) : center(center), radius(radius),
+				const Vec3f &emission_color, const float &k_transparency,
+				const float &k_reflection) : center(center), radius(radius),
 				surface_color(surface_color), emission_color(emission_color),
 				k_transparency(k_transparency), k_reflection(k_reflection)
 {
@@ -55,6 +55,22 @@ Vec3f Sphere::trace(const Vec3f &orig_raio, const Vec3f &dir_raio,
     {
 			return Vec3f(2,2,2);
 		}
+		else{
+			Vec3f surface_color(0, 0, 0); //cor do raio/superficie do objeto interseccionado pelo raio
+			Vec3f p_hit = orig_raio + dir_raio * near; //equacao da reta do ponto de interseccao
+			Vec3f p_normal = p_hit - sphere->center; //reta normal no ponto de interseccao
+			p_normal.normalize(); //normalizar a normal :P para trabalhar com "versores"
+			//Se a direcao da normal e da visao nao sao opostas, significa que estamos dentro da esfera
+			// portanto, devemos inverter a direcao
 
-		
+			float bias = 1e-4; //para trabalhar com float, eh bom criar um vies em comparacoes
+			bool is_inside = false; //assumimos estarmos fora da esfera
+
+			if (dir_raio.prod_interno(p_normal) > 0){ //visao e normal na mesma direcao
+				p_normal = p_normal.operator-(p_normal);
+				std::cout << "p_normal  " << p_normal << std::endl;
+				is_inside = true;
+			}
+
+		}
 }
